7be39fa7d85d7246e11d8a4489c738f48dbaf745
Phablo Vilas Boas <phablo_sena@hotmail.com>
2022-01-04 06:22:51 -0300

    finish

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,35 @@
-export function SideBar() {
-  // Complete aqui
+type Props = {
+  genres: Array<{
+    id: number;
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    title: string;
+  }>;
+  setSelectedGenreId: (id: number) => void;
+  selectedGenreId: number;
+};
+
+import { Button } from "./Button";
+
+export function SideBar({ genres, setSelectedGenreId, selectedGenreId }: Props) {
+
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  return (
+    <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+        <div className="buttons-container">
+          {genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => handleClickButton(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+    </nav>
+  );
 }
